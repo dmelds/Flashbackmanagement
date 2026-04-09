16fda7b5ec90d7b037a073bf3db7fc66feaa460d
dmelds <dmelds@gmail.com>
2026-04-09 10:14:13 -0500

    Add service worker with caching configuration

---
+++ service-worker.js
@@ -0,0 +1,11 @@
+const CACHE_NAME = 'flashback-steps-v2-no-header';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './service-worker.js',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('
